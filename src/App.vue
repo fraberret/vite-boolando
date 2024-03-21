ccba6d3dbe061c361ef415f0f7b3b54320febd6a
fraberret <f.berretta02@gmail.com>
2024-03-21 15:48:18 +0100

    add AppFooter component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppMain
+    AppMain,
+    AppFooter
   }
 }
 </script>
@@ -14,6 +16,7 @@ export default {
 <template>
   <AppHeader />
   <AppMain />
+  <AppFooter />
 </template>
 
 <style></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<template>
+    <footer id="site_footer">
+        <div class="footer_contenitor contenitor">
+            <div class="copyright">
+                <h3 class="padding-bottom">Boolando s.r.l</h3>
+                <div class="info">
+                    <ul>
+                        <li>
+                            <a href="">Informazioni legali</a>
+                        </li>
+                        <li>
+                            <a href="">Informativa sulla privacy</a>
+                        </li>
+                        <li>
+                            <a href="">Diritto di recesso</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="social">
+                <p class="padding-bottom">Trovaci anche su</p>
+                <div class="social_icon">
+                    <ul>
+                        <li>
+                            <a href="">
+                                <i class="fa-brands fa-square-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fa-brands fa-square-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fa-brands fa-square-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fa-brands fa-square-pinterest"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fa-brands fa-square-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style></style>
