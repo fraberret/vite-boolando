48c9fbdc3d73e85339bc5c320e3b14f78d4cab74
fraberret <f.berretta02@gmail.com>
2024-03-22 12:03:52 +0100

    add card products in App main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,88 +1,17 @@
 <script>
+import CardProducts from './CardProducts.vue'
 export default {
     name: 'AppMain',
+    components: {
+        CardProducts
+    }
 }
 </script>
 
 <template>
-    <main id="site_main">
+    <main>
         <div class="main_contenitor contenitor">
-            <div class="item levis_first">
-                <img class="first_img" src="/images/1.webp" alt="">
-                <img class="second_img" src="/images/1b.webp" alt="">
-                <div class="heart_square"><i class="fa-solid fa-heart"></i>
-                </div>
-                <div class="red_square">-50%</div>
-                <div class="green_square">Sostenibilit&aacute;</div>
-                <div class="item_description">
-                    <p class="brand small">Levi's</p>
-                    <p class="object"><strong>RELAXED FIT TEE UNISEX</strong></p>
-                    <span class="discount_price small">14,99&euro;</span>
-                    <span class="real_price small">29,99&euro;</span>
-
-                </div>
-            </div>
-
-            <div class="item guess">
-                <div class="heart_square"><i class="fa-solid fa-heart"></i></div>
-                <div class="red_square">-30%</div>
-                <div class="item_description">
-                    <p class="brand small">Guess</p>
-                    <p class="object"><strong>ROSES TEE</strong></p>
-                    <span class="discount_price small">20,99&euro;</span>
-                    <span class="real_price small">29,99&euro;</span>
-
-                </div>
-            </div>
-
-            <div class="item third_item">
-                <div class="heart_square"><i class="fa-solid fa-heart"></i></div>
-                <div class="red_square">-30%</div>
-                <div class="item_description">
-                    <p class="brand small">Come Zucchero Filato</p>
-                    <p class="object"><strong>VOGLIA DI COLORI PASTELLO</strong></p>
-                    <span class="discount_price small">129,99&euro;</span>
-                    <span class="real_price small">184,99&euro;</span>
-
-                </div>
-            </div>
-
-            <div class="item levis_forth">
-                <div class="heart_square"><i class="fa-solid fa-heart"></i></div>
-                <div class="red_square">-50%</div>
-                <div class="green_square">Sostenibilit&aacute;</div>
-                <div class="item_description">
-                    <p class="brand small">Levi's</p>
-                    <p class="object"><strong>TEE UNISEX</strong></p>
-                    <span class="discount_price small">14,99&euro;</span>
-                    <span class="real_price small">29,99&euro;</span>
-
-                </div>
-            </div>
-
-            <div class="item long_dress">
-                <div class="heart_square"><i class="fa-solid fa-heart"></i></div>
-                <div class="item_description">
-                    <p class="brand small">Maya Deluxe</p>
-                    <p class="object"><strong>STRIPE BODICE</strong></p>
-                    <span class="discount_price small">99,99&euro;</span>
-
-
-                </div>
-            </div>
-
-            <div class="item last_item">
-                <div class="heart_square"><i class="fa-solid fa-heart"></i></div>
-                <div class="green_square bottom-left">Sostenibilit&aacute;</div>
-                <div class="item_description">
-                    <p class="brand small">Espirit</p>
-                    <p class="object"><strong>MAGLIONE - BLACK</strong></p>
-                    <span class="discount_price small">29,99&euro;</span>
-
-
-                </div>
-            </div>
-
+            <CardProducts />
         </div>
     </main>
 </template>
---
+++ src/components/CardProducts.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="item">
+        <img class="first_img" src="/images/1.webp" alt="">
+        <img class="second_img" src="/images/1b.webp" alt="">
+        <div class="heart_square"><i class="fa-solid fa-heart"></i>
+        </div>
+        <div class="red_square">-50%</div>
+        <div class="green_square">Sostenibilit&aacute;</div>
+        <div class="item_description">
+            <p class="brand small">Levi's</p>
+            <p class="object"><strong>RELAXED FIT TEE UNISEX</strong></p>
+            <span class="discount_price small">14,99&euro;</span>
+            <span class="real_price small">29,99&euro;</span>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardProducts'
+}
+</script>
+
+<style></style>
